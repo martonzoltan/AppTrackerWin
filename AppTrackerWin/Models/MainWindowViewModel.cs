4c4e7fed9ebf05d3a6554a5e81c095fd7f6a6207
Zoltan Marton <martonzoltan4@gmail.com>
2020-02-15 20:42:51 +0200

    finished command relay for event based user control switching

--- AppTrackerWin/Models/MainWindowViewModel.cs
+++ AppTrackerWin/Models/MainWindowViewModel.cs
@@ -1,22 +1,71 @@
-﻿using System;
+﻿using AppTrackerWin.Helper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace AppTrackerWin.Models
 {
-    public class MainWindowViewModel
-    {
-        public int SwitchView
-        {
-            get;
-            set;
-        }
+    public class MainWindowViewModel : ViewModelBase
+   {
+      private ICommand _gotoView1Command;
+      private ICommand _gotoView2Command;
+      private object _currentView;
+      private object _view1;
+      private object _view2;
+ 
+      public MainWindowViewModel()
+      {
+            _view1 = new ExcelTrackingList();
+            _view2 = new ExcelDatabaseManagement();
 
-        public MainWindowViewModel()
-        {
-            SwitchView = 0;
-        }
-    }
+         CurrentView = _view1;
+      }
+ 
+      public object GotoView1Command
+      {
+         get
+         {
+            return _gotoView1Command ?? (_gotoView1Command = new RelayCommand(
+               x =>
+               {
+                  GotoView1();
+               }));
+         }
+      }
+ 
+      public ICommand GotoView2Command
+      {
+         get
+         {
+            return _gotoView2Command ?? (_gotoView2Command = new RelayCommand(
+               x =>
+               {
+                  GotoView2();
+               }));
+         }
+      }
+ 
+      public object CurrentView
+      {
+         get { return _currentView; }
+         set
+         {
+            _currentView = value;
+            OnPropertyChanged("CurrentView");
+         }
+      }
+ 
+      private void GotoView1()
+      {
+         CurrentView = _view1;
+      }
+ 
+      private void GotoView2()
+      {
+         CurrentView =  _view2;
+      }
+   }
 }
